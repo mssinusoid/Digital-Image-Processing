070c6fd5ab4dcc10d956991fa52abd7097e8798c
Khanh-Huyen <147962863+mssinusoid@users.noreply.github.com>
2023-11-23 16:33:13 +0700

    axis off

--- mean_filter/arithmetic_mean_filter.py
+++ mean_filter/arithmetic_mean_filter.py
@@ -6,7 +6,7 @@ impulse_noise_img = cv2.imread('mean_filter\gauss_noise.png', 0)\
 
 def mean_filter(img, n=3):
     h, w = img.shape
-    restored_img = np.zeros_like(img)
+    restored_img = np.copy(img)
     for i in range(h-n//2):
         for j in range(w-n//2):
             neighbors = [img[i,j]]
@@ -24,13 +24,14 @@ def mean_filter(img, n=3):
 
 restored_img = mean_filter(impulse_noise_img)
 
-plt.axis('off')
 plt.subplot(121)
 plt.imshow(impulse_noise_img, cmap = "gray")
 plt.title('Before')
+plt.axis('off')
 
 plt.subplot(122)
 plt.imshow(restored_img, cmap = "gray")
 plt.title('After')
+plt.axis('off')
 
 plt.show()
